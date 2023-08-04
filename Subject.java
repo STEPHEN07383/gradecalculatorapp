ee10c6e5786850f69eb2bad2c7b9ac7ab445890f
Stephen ombuya <129269603+STEPHEN07383@users.noreply.github.com>
2023-08-04 14:54:10 +0300

    Add files via upload

---
+++ Subject.java
@@ -0,0 +1,13 @@
+package gradecalculatorapp;
+
+public class Subject {
+    private double score;
+
+    public Subject(double score) {
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+}
